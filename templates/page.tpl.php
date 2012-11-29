2a2281cde4f7815051cdf9c4ac356a9118635637
Randy Franklin <randyf@stanford.edu>
2012-11-28 17:59:11 -0800

    Updating general layout with header and footer placeholder images.   Updating page.tpl.php with appropriate bootstrap-specific classes.  Clean-up of info file.

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -10,30 +10,53 @@
   </div>
 </div>
 <!-- /#skipnav -->
-<?php if ($logo || $site_name || $site_slogan || ($page['header']) || ($page['navigation']) || ($search)): ?>
+<?php if ($logo || $site_name || $main_menu || $site_slogan || ($page['header']) || ($page['navigation']) || ($search)): ?>
 <div id="header" class="clearfix">
   <div class="container">
     <div class="row">
-      <div class="<?php if (($page['navigation']) || ($search)): print 'span8'; else: print 'span12'; endif; ?>">
+      <div class="span12">
         <?php if ($logo): ?>
-        <div id="logo"> <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"> <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" role="presentation" /> </a></div>
+        <div id="logo" class="span3"> <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"> <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" role="presentation" /> </a></div>
         <!-- /#logo -->
         <?php endif; ?>
         <?php if ($site_name || $site_slogan): ?>
-        <div id="name-and-slogan">
+        <div id="name-and-slogan" class="offset2 span6">
           <?php if ($site_name): ?>
-          <div id="site-name"><a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><?php print $site_name; ?></a></div>
+          <div id="site-name" class=""><a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><?php print $site_name; ?></a></div>
           <?php endif; ?>
+
+         <!-- #main-menu -->
+                <?php if ($main_menu): ?>
+          <div id="main-menu" class="clearfix span6">
+          
+            <div class="navbar">
+              <div class="navbar-inner">
+                <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse"> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </a>
+                <div class="nav-collapse"><?php print render($main_menu_expanded); ?></div>
+              </div>
+            </div>
+          
+          </div><?php endif; ?>
+        <!-- /#main-menu -->
+
+
           <?php if ($site_slogan): ?>
           <div id="site-slogan"><?php print $site_slogan; ?></div>
           <?php endif; ?>
+
+
+
         </div>
+
         <!-- /#name-and-slogan -->
         <?php endif; ?>
         <?php if ($page['header']): ?>
         <div id="header-content" class="row-fluid"><?php print render($page['header']); ?></div>
         <!-- /#header-content -->
         <?php endif; ?>
+
+
+      <?php endif; ?>
       </div>
       <?php if ($page['navigation']): ?>
       <div id="navigation" class="span4"> <?php print render($page['navigation']); ?> </div>
@@ -42,25 +65,8 @@
   </div>
 </div>
 <!-- /#header -->
-<?php endif; ?>
-<?php if ($main_menu): ?>
-<div id="main-menu" class="clearfix">
-  <div class="container">
-    <div class="navbar">
-      <div class="navbar-inner">
-        <?php if ($search): ?>
-        <div id="nav-search">
-          <?php if ($search): print render($search); endif; ?>
-        </div>
-        <?php endif; ?>
-        <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse"> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </a>
-        <div class="nav-collapse"><?php print render($main_menu_expanded); ?></div>
-      </div>
-    </div>
-  </div>
-</div>
-<!-- /#main-menu -->
-<?php endif; ?>
+
+
 <div id="main" class="clearfix">
   <div class="container">
     <?php if ($breadcrumb): ?>
@@ -79,7 +85,7 @@
       </div>
       <!-- /#sidebar-first -->
       <?php endif; ?>
-      <div id="content" class="<?php if (($page['sidebar_first']) && ($page['sidebar_second'])): print 'span6'; elseif (($page['sidebar_first']) || ($page['sidebar_second'])): print 'span9'; else: print 'span12'; endif; ?>">
+      <div id="content">
         <div id="content-wrapper">
           <div id="content-head" class="row-fluid">
             <div id="highlighted" class="clearfix"><?php print render($page['highlighted']); ?></div>
@@ -103,37 +109,17 @@
             </ul>
             <?php endif; ?>
           </div>
-          <?php if ($page['content_top']): ?>
-          <div id="content-top" class="row-fluid"> <?php print render($page['content_top']); ?> </div>
-          <?php endif; ?>
-          <?php if ($page['content_upper']): ?>
-          <div id="content-upper" class="row-fluid"> <?php print render($page['content_upper']); ?> </div>
-          <?php endif; ?>
+         
           <div id="content-body" class="row-fluid"> <?php print render($page['content']); ?> <?php print $feed_icons; ?> </div>
           
-          <?php if ($page['content_lower']): ?>
-          <div id="content-lower" class="row-fluid"> <?php print render($page['content_lower']); ?> </div>
-          <?php endif; ?>
-          <?php if ($page['content_bottom']): ?>
-          <div id="content-bottom" class="row-fluid"> <?php print render($page['content_bottom']); ?> </div>
-          <?php endif; ?>
+       
         </div>
         <!-- /#content-wrap --> 
       </div>
       <!-- /#content -->
-      <?php if ($page['sidebar_second']): ?>
-      <div id="sidebar-second" class="sidebar span3">
-        <div class="row-fluid"><?php print render($page['sidebar_second']); ?></div>
-      </div>
-      <!-- /#sidebar-second -->
-      <?php endif; ?>
+      
     </div>
-    <?php if ($page['main_lower']): ?>
-    <div id="main-lower" class="row-fluid"> <?php print render($page['main_lower']); ?> </div>
-    <?php endif; ?>
-    <?php if ($page['main_bottom']): ?>
-    <div id="main-bottom" class="row-fluid"> <?php print render($page['main_bottom']); ?> </div>
-    <?php endif; ?>
+    
   </div>
 </div>
 <!-- /#main, /#main-wrapper -->
