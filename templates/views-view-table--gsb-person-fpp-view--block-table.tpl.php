ddea566dc6e6167b8680e4a400c37b4754642d17
Dumitru Grosul <dumitru.grosul@wearepropeople.com>
2013-02-04 10:09:14 -0800

    Issue: Person block theming

---
+++ templates/views-view-table--gsb-person-fpp-view--block-table.tpl.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * @file
+ * Template to display a view as a table.
+ *
+ * - $title : The title of this group of rows.  May be empty.
+ * - $header: An array of header labels keyed by field id.
+ * - $header_classes: An array of header classes keyed by field id.
+ * - $fields: An array of CSS IDs to use for each field id.
+ * - $classes: A class or classes to apply to the table, based on settings.
+ * - $row_classes: An array of classes to apply to each row, indexed by row
+ *   number. This matches the index in $rows.
+ * - $rows: An array of row items. Each row is an array of content.
+ *   $rows are keyed by row number, fields within rows are keyed by field ID.
+ * - $field_classes: An array of classes to apply to each field, indexed by
+ *   field id, then row number. This matches the index in $rows.
+ * @ingroup views_templates
+ */
+?>
+<div class="designed-box" <?php if ($classes) { print 'class="'. $classes . '" '; } ?><?php print $attributes; ?>>
+  <div class="person-table-inner">
+    <?php foreach ($rows as $row_count => $row): ?>
+      <?php foreach ($row as $field => $content): ?>
+          <div class="gsb-person-feature-row" <?php if ($field_classes[$field][$row_count]) { print 'class="'. $field_classes[$field][$row_count] . '" '; } ?><?php print drupal_attributes($field_attributes[$field][$row_count]); ?>>
+            <?php print $content; ?>
+          </div>
+        <?php endforeach; ?>
+    <?php endforeach; ?>
+  </div>
+</div>
