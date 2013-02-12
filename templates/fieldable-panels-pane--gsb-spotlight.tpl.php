d134cef4c3bfa1d796eea18c47edeac58228ea0d
Dumitru Grosul <dumitru.grosul@wearepropeople.com>
2013-02-12 11:35:15 -0800

    Issue: Spotlight block theming, events sidebar and frontpage theming

--- templates/fieldable-panels-pane--gsb-spotlight.tpl.php
+++ templates/fieldable-panels-pane--gsb-spotlight.tpl.php
@@ -1,3 +1,4 @@
+<?php // dsm(get_defined_vars()) ?>
 <div class="designed-box spotlight spotlight-<?php print strtolower($field_gsb_spotlight_person_type[0]['value']) ?>">
   <span class="spotlight-border"></span>
   <span class="spotlight-type"><?php print $field_gsb_spotlight_person_type[0]['value'] ?> Spotlight</span>
@@ -9,31 +10,37 @@
     	<h5 class="spotlight-program"><?php print $content['field_program'][0]['#markup'] . ", Class of " . $content['field_graduation_year'][0]['#markup']; ?></h5>
     <?php } ?>
   <div class="spotlight-image">
-  	    <?php
+  	<?php
       // Make the file information an object.
-      $file = (object) $field_gsb_spotlight_image_video[0];
-      if ($file->type == 'image') {
-        print render($content['field_gsb_spotlight_image_video']);
-      }
-      elseif ($file->type == 'video') {
-        // Get the video path.
-        $video_path = drupal_realpath($file->uri);
-        // If it's a youtube video we need to use the embed url.
-        if ($field_gsb_spotlight_image_video[0]['filemime'] == 'video/youtube') {
-          $video_parts = drupal_parse_url($video_path);
-          if (stristr($video_parts['path'], 'watch')) {
-          	$video_path = str_replace('watch', 'embed', $video_parts['path']) . "/" . $video_parts['query']['v'];
-          }
-          $video_path .= '?autoplay=1';
-          //$video_path = str_replace('watch?v=', 'embed/', $video_path) . '?autoplay=1';
+      if ($field_gsb_spotlight_use_prof_img[0]['value'] == 1 && $field_gsb_spotlight_person_type[0]['value'] == 'Faculty') {
+        $personame = $field_first_name[0]['safe_value'] . " " . $field_last_name[0]['safe_value'];
+        $profileimage = file_load($field_gsb_spotlight_profile_fid[0]['value']);
+        print '<img src="' . image_style_url('medium', $profileimage->uri) . '" class="person-thumb" title="' . $personame . '">';
+      } else {
+        $file = (object) $field_gsb_spotlight_image_video[0];
+        if ($file->type == 'image') {
+          print render($content['field_gsb_spotlight_image_video']);
         }
-        // Create the image.
-        render($content['field_gsb_spotlight_image_video']);
-        $img_tag = theme_image_style(array('style_name' => $content['field_gsb_spotlight_image_video'][0]['file']['#style_name'], 'path' => $content['field_gsb_spotlight_image_video'][0]['file']['#path'], 'alt' => $field_gsb_spotlight_image_video[0]['filename'], 'width' => NULL, 'height' => NULL));
-        // Print a link wrapper so that it can open in a modal.
-        print '<div class="field field-type-image field-name-field-gsb-spotlight-media">';
-        print l($img_tag, $video_path, array('attributes' => array('class' => 'fancybox fancybox.iframe'), 'html' => TRUE));
-        print '<span class="video-play-icon"></span></div>';
+        elseif ($file->type == 'video') {
+          // Get the video path.
+          $video_path = drupal_realpath($file->uri);
+          // If it's a youtube video we need to use the embed url.
+          if ($field_gsb_spotlight_image_video[0]['filemime'] == 'video/youtube') {
+            $video_parts = drupal_parse_url($video_path);
+            if (stristr($video_parts['path'], 'watch')) {
+            	$video_path = str_replace('watch', 'embed', $video_parts['path']) . "/" . $video_parts['query']['v'];
+            }
+            $video_path .= '?autoplay=1';
+            //$video_path = str_replace('watch?v=', 'embed/', $video_path) . '?autoplay=1';
+          }
+          // Create the image.
+          render($content['field_gsb_spotlight_image_video']);
+          $img_tag = theme_image_style(array('style_name' => $content['field_gsb_spotlight_image_video'][0]['file']['#style_name'], 'path' => $content['field_gsb_spotlight_image_video'][0]['file']['#path'], 'alt' => $field_gsb_spotlight_image_video[0]['filename'], 'width' => NULL, 'height' => NULL));
+          // Print a link wrapper so that it can open in a modal.
+          print '<div class="field field-type-image field-name-field-gsb-spotlight-media">';
+          print l($img_tag, $video_path, array('attributes' => array('class' => 'fancybox fancybox.iframe'), 'html' => TRUE));
+          print '<span class="video-play-icon"></span></div>';
+        }
       }
     ?>
   </div>
---
+++ templates/views-view-field--gsb-event--sidebar--field-event-date.tpl.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * @file
+ * This template is used to print a single field in a view.
+ *
+ * It is not actually used in default Views, as this is registered as a theme
+ * function which has better performance. For single overrides, the template is
+ * perfectly okay.
+ *
+ * Variables available:
+ * - $view: The view object
+ * - $field: The field handler object that can process the input
+ * - $row: The raw SQL result that can be used
+ * - $output: The processed output that will normally be used.
+ *
+ * When fetching output from the $row, this construct should be used:
+ * $data = $row->{$field->field_alias}
+ *
+ * The above will guarantee that you'll always get the correct data,
+ * regardless of any changes in the aliasing that might happen if
+ * the view is modified.
+ */
+?>
+<?php 
+$dates = explode(" to ", $field->original_value);
+$date1 = strtotime($dates['0']);
+$date2 = strtotime($dates['1']);
+$output = '';
+
+if (date('o M j', $date1) != date('o M j', $date2)) {
+	$output .= '<span>'
+	. date('l, ', $date1)
+	. '</span>'
+	. strtoupper(date('M j', $date1)) 
+	. ' – ' 
+	. '<span>'
+	. date('l, ', $date2)
+	. '</span>'
+	. strtoupper(date('M j', $date2));
+} else {
+	if ($row->field_field_all_day_event['0']['raw']['value'] == 1) {
+		$output .= date('l, ', $date1)
+		. strtoupper(date('M j', $date1));
+	} else {
+		$output .= '<span>'
+		. date('l, ', $date1)
+		. '</span>'
+		. strtoupper(date('M j', $date1)) 
+		. date(', ga', $date1) 
+		. ' – ' 
+		. date('ga', $date2);
+	}
+}
+?>
+
+
+<?php print $output; ?>
