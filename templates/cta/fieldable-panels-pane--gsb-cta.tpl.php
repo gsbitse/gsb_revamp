ecc59659004bbc671c55dcdd6d89cfdb76de6428
kirill_cebotari <kirill.cebotari@wearepropeople.com>
2013-02-28 12:29:09 -0800

    Issue: Slideshow and CTA theming

--- templates/cta/fieldable-panels-pane--gsb-cta.tpl.php
+++ templates/cta/fieldable-panels-pane--gsb-cta.tpl.php
@@ -1,11 +1,19 @@
 <?php 
-  print sprintf('<div class="%s">', $field_cta_style[0]['value']);
-  if ($field_cta_title[0]['value']) {
-    print '<div class="title">' . $field_cta_title[0]['value'] . '</div>';
-  } 
-  if ($field_cta_description[0]['safe_value']) {
-    print '<div class="description">' . $field_cta_description[0]['safe_value'] . '</div>';
-  }
-  print sprintf('<div class="cta-link"><a href="%s">%s</a></div>', $field_cta_link[0]['url'], $field_cta_link[0]['title']);
+	if (!empty($field_cta_link[0]['title'])) {
+	  print sprintf('<div class="cta-block %s">', $field_cta_style[0]['value']);
+	  $singleclass = empty($field_cta_description[0]['safe_value']) ? 'single' : '';
+
+	  print sprintf('<a href="%s" class="cta-link %s">%s<i></i></a>', $field_cta_link[0]['url'], $singleclass, $field_cta_link[0]['title']);
+	  if ($field_cta_description[0]['safe_value']) { 
+	  	print '<div class="designed-box cta-description">';
+		  if ($field_cta_title[0]['value']) {
+		    print '<div class="cta-title">' . $field_cta_title[0]['value'] . '</div>';
+		  } 
+		  print '<div class="description">' . $field_cta_description[0]['safe_value'] . '</div></div>';
+
+	  }
+	}
 
   print '</div>';
+
+  
