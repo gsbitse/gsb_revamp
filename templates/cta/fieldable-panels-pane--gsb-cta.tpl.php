2198c8cdbd19cd6a432cbe7a84fbc6e7036d5cb6
kirill_cebotari <kirill.cebotari@wearepropeople.com>
2013-03-01 11:09:06 -0800

    Issue: CTA title optional fix

--- templates/cta/fieldable-panels-pane--gsb-cta.tpl.php
+++ templates/cta/fieldable-panels-pane--gsb-cta.tpl.php
@@ -1,8 +1,12 @@
 <?php 
-	if (!empty($field_cta_link[0]['title'])) {
-	  print sprintf('<a href="%s" class="designed-box %s cta-link"><span class="cta-title">%s</span><b>%s', $field_cta_link[0]['url'], $field_cta_style[0]['value'], $field_cta_title[0]['value'], $field_cta_link[0]['title']);
-		print '</b><i></i></a>';  
-	}
+  if (!empty($field_cta_link[0]['title'])) {
+    $title = isset($field_cta_title) ? sprintf('<span class="cta-title">%s</span>', $field_cta_title[0]['value']) : '';
+
+    printf('<a href="%s" class="designed-box %s cta-link">%s<b>%s</b><i></i></a>', 
+      $field_cta_link[0]['url'], 
+      $field_cta_style[0]['value'],
+      $title, 
+      $field_cta_link[0]['title']);
+  }
 
 
-  
