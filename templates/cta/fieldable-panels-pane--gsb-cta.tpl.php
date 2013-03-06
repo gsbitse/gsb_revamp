dfe7322b19c968504d02e59b59cd0c10737ec061
creaoy <jk@propeople.com.ua>
2013-03-05 17:23:29 -0800

    get styles for cta

--- templates/cta/fieldable-panels-pane--gsb-cta.tpl.php
+++ templates/cta/fieldable-panels-pane--gsb-cta.tpl.php
@@ -1,5 +1,17 @@
 <?php 
-  if (!empty($field_cta_link[0]['title'])) {
+
+
+  echo $variables['field_cta_style'][0]['value'];
+
+  foreach ($variables['field_cta_item'] as $key => $cta_item) {
+
+    $title = $variables['elements']['field_cta_item'][$key]['entity']['field_collection_item'][$cta_item['value']]['field_cta_destination'][0]['#markup'];
+    $link = $variables['elements']['field_cta_item'][$key]['entity']['field_collection_item'][$cta_item['value']]['field_cta_destination_link'][0]['#markup'];
+
+	$action = $variables['elements']['field_cta_item'][$key]['entity']['field_collection_item'][$cta_item['value']]['field_cta_action'][0]['#markup'];
+  }
+
+/*
     $title = isset($field_cta_title) ? sprintf('<span class="cta-title">%s</span>', $field_cta_title[0]['value']) : '';
 
     printf('<a href="%s" class="designed-box %s cta-link">%s<b>%s</b><i></i></a>', 
@@ -7,4 +19,4 @@
       $field_cta_style[0]['value'],
       $title, 
       $field_cta_link[0]['title']);
-  }
+      */
