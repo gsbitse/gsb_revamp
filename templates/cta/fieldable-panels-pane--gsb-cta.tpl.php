90276094cfac7ecf863617ca04848cbc57386fbc
kirill_cebotari <kirill.cebotari@wearepropeople.com>
2013-03-08 13:30:11 -0800

    Issue: CTA theming

--- templates/cta/fieldable-panels-pane--gsb-cta.tpl.php
+++ templates/cta/fieldable-panels-pane--gsb-cta.tpl.php
@@ -1,22 +1,17 @@
 <?php 
 
-
-  echo $variables['field_cta_style'][0]['value'];
-
+  $variables['field_cta_style'][0]['value'];
+  print '<div class="cta-wrapper designed-box">';
   foreach ($variables['field_cta_item'] as $key => $cta_item) {
 
     $title = $variables['elements']['field_cta_item'][$key]['entity']['field_collection_item'][$cta_item['value']]['field_cta_destination'][0]['#markup'];
     $link = $variables['elements']['field_cta_item'][$key]['entity']['field_collection_item'][$cta_item['value']]['field_cta_destination_link'][0]['#markup'];
 
 	$action = $variables['elements']['field_cta_item'][$key]['entity']['field_collection_item'][$cta_item['value']]['field_cta_action'][0]['#markup'];
-  }
-
-/*
-    $title = isset($field_cta_title) ? sprintf('<span class="cta-title">%s</span>', $field_cta_title[0]['value']) : '';
-
-    printf('<a href="%s" class="designed-box %s cta-link">%s<b>%s</b><i></i></a>', 
-      $field_cta_link[0]['url'], 
+  printf('<a href="%s" class="%s cta-link"><span>%s</span><b>%s</b><i></i></a>', 
+      $link, 
       $field_cta_style[0]['value'],
-      $title, 
-      $field_cta_link[0]['title']);
-      */
+      $action, 
+      $title);
+  }
+  print '</div>';
